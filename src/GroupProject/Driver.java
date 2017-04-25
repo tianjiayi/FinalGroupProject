b5cc4ca11fb21c248eece64e44f115e77d099be2
tt <tianjiayitianhong@gmail.com>
2017-04-25 01:41:43 -0400

    Driver connection to mysql database was tested successful

--- src/GroupProject/Driver.java
+++ src/GroupProject/Driver.java
@@ -1,13 +1,16 @@
 package GroupProject;
 import java.sql.*;
 import com.mysql.jdbc.Connection;
+import java.sql.DriverManager;
 public class Driver {
 
-	public static void main(String[] args) {
-		// Driver for mySql
+	public static void main(String[] args) throws Exception{
+		getConnection();
+				
+	}
+	/*	// Driver for mySql
 		Connection myConn = null;
-		try{
-		      
+		try{		      
 		    	    // Load the JDBC driver
 		    	    Class.forName("com.mysql.jdbc.Driver");
 		    	    System.out.println("Driver loaded");
@@ -18,38 +21,62 @@ public class Driver {
 			
 			if(myConn !=null){
 				System.out.println("Connected successfully to database.");
-			}
-			
-			
-			
-			
-			
-			
+			}		
 			//2. create a statement
 			Statement myStat = myConn.createStatement();
 			//3. execute SQL query
 			ResultSet myRs = myStat.executeQuery("select * from flight");
 			//4. process the result set
 			while(myRs.next()){
-				System.out.println(myRs.getString("destination") + myRs.getString("flightCode"));
-				
-				
+				System.out.println(myRs.getString("destination") + myRs.getString("flightCode"));				
 				//5. Close the connection
-			    myConn.close();
-
-				
+			    myConn.close();				
 			}
-			
-			
-			
-			
-			
+						
 		}catch(Exception ex){
 			
-			ex.printStackTrace();
+			ex.printStackTrace();			
+		}}}*/
+		
+		
+	public static Connection getConnection() throws Exception{
+		try{
+			String driver = "com.mysql.jdbc.Driver";
+			String url = "jdbc:mysql://127.0.0.1:3306/airlineReservationDB";
+			String username = "root";
+			String password = "1";
+			Class.forName(driver);
+			
+			Connection conn = (Connection)DriverManager.getConnection (url,username, password);
+			System.out.println("Connected");
+			
+			
+			
+			//2. create a statement
+			Statement myStat = conn.createStatement();
+			//3. execute SQL query
+			ResultSet myRs = myStat.executeQuery("select * from flight");
+			//4. process the result set
+			while(myRs.next()){
+				System.out.println(myRs.getString("destination") + "   ,   "+ myRs.getString("flightCode"));
+				
+								
+			}
+			
+						
+			
+		}catch(Exception ex){
+			System.out.println(ex);
+			
 			
 		}
-
+			
+		
+		
+		return null;
 	}
-
+		
+		
+		
+		
 }
